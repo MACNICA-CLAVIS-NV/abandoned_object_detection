33fab6f8ce2f53e5dba6e196c3ad1e7a5d83bb84
tfuru-macnica <57699379+tfuru-macnica@users.noreply.github.com>
2021-04-14 13:56:40 +0900

    Add files via upload

---
+++ config/AOD_config.py
@@ -0,0 +1,15 @@
+
+# approximate object dimensions in inches
+SUITCASE_WIDTH = 27  # inches
+BACKPACK_WIDTH = 13  # inches
+HANDBAG_WIDTH = 15   # inches
+
+# no. of seconds for which an object will be checked for abandonment
+# if there are surrounding people present in the frame
+ABANDONMENT_DURATION = 10  # seconds
+
+# no. of seconds for which the initial object-person
+# distances will be computed
+INITIAL_INSPECTION_DURATION = 5  # seconds
+REFERENCE_OBJECTS = ['suitcase', 'handbag', 'backpack']
+
